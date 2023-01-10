b64b9ccd65e941cc4368955cc45d19d2fa99652e
AdamHellberg <adam.hellberg@elev.ga.ntig.se>
2023-01-10 13:02:24 +0100

    min inlämning
    sista pushen

--- aboutus.js
+++
@@ -1,8 +0,0 @@
-var div = document.querySelector('#sec1')
-window.addEventListener("scroll", function(){
-    if (window.scrollY >= 100) {
-    div.getElementsByClassName.opacity = 0;
-    } else {
-    div.getElementsByClassName.opacity = 1;
-    }
-});
