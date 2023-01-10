ec4ecf1756d8279234c98d7f274f2d0e68bee35f
AdamHellberg <adam.hellberg@elev.ga.ntig.se>
2023-01-10 10:36:30 +0100

    sista pushen
    sista pushen

---
+++ aboutus.js
@@ -0,0 +1,8 @@
+var div = document.querySelector('#sec1')
+window.addEventListener("scroll", function(){
+    if (window.scrollY >= 100) {
+    div.getElementsByClassName.opacity = 0;
+    } else {
+    div.getElementsByClassName.opacity = 1;
+    }
+});
